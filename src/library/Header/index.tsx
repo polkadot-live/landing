38a20aeb20d6d9eff0fd925226557db2efda7587
Ross Bulat <ross@jkrb.io>
2024-07-20 11:04:27 +0100

    chore: rm version

--- src/library/Header/index.tsx
+++ src/library/Header/index.tsx
@@ -5,7 +5,6 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { GithubRepoUrl } from 'consts';
 import { HeaderWrapper } from './Wrapper';
-import { version } from '../../../package.json';
 import { Link } from 'react-router-dom';
 
 export const Header = () => (
@@ -18,7 +17,6 @@ export const Header = () => (
           <img src="/png/HeaderLogo.png" style={{ height: '1.5rem' }} />
         </Link>
       </span>
-      <span className="version">{version}</span>
     </div>
     <div>
       <button type="button" onClick={() => window.open(GithubRepoUrl)}>
